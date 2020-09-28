d6ea5c144d6cf98d90a0e47fdc56995bd6e0381f
Chmouss <61546233+Chmouss@users.noreply.github.com>
2020-09-28 18:17:56 +0200

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 <div id="contenu">
-	<h2>TEST mais ça ne marche pas visiblement</h2>
+	<h2>Dorian nik toi</h2>
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 	<p>Jean-Luc Collinet.</p>
 </div><!-- #contenu -->
