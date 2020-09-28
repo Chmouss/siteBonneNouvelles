e43f8c1b3febad1fb4ff58c509eaaafc828532af
Chmouss <61546233+Chmouss@users.noreply.github.com>
2020-09-28 18:19:09 +0200

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 <div id="contenu">
-	<h2>Dorian nik toi</h2>
+	<h2>RUSH B CYKA</h2>
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 	<p>Jean-Luc Collinet.</p>
 </div><!-- #contenu -->
